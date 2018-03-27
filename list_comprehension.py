91a420e002d2796443911d6bc0aedbffb20e031e
Abhishek Rana <ar10695@gmail.com>
2018-03-27 20:29:55 +0530

    program to show list comrehension

--- list_comprehension.py
+++ list_comprehension.py
@@ -1,11 +1,11 @@
-squares=[]
+cubes=[]
 
 for x in range(1,11):
-    squares.append(x**3)
+    cubes.append(x**3)
 
 print squares
 
-squares=[x*2 for x in range(2,11)]
+doubles=[x*2 for x in range(2,11)]
 print squares
 
 triple=lambda x: x*3
